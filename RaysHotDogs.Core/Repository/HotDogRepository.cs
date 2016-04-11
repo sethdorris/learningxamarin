77541009d1429c794483ae1f5782d91a1b5be818
sethdorris <sethc.dorris@gmail.com>
2016-04-11 14:56:49 -0600

    Built initial repository

---
+++ RaysHotDogs.Core/Repository/HotDogRepository.cs
@@ -0,0 +1,120 @@
+﻿using RaysHotDogs.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaysHotDogs.Core.Repository
+{
+    public class HotDogRepository
+    {
+        private static List<HotDogGroup> hotDogGroups = new List<HotDogGroup>()
+        {
+            new HotDogGroup()
+            {
+                HotDogGroupID = 1, Title = "Meat Lovers", ImagePath = "", HotDogs = new List<HotDog>()
+                {
+                    new HotDog()
+                    {
+                        HotDogId = 1,
+                        Name = "Regular Hot Dog",
+                        ShortDescription = "The best there is on the planet",
+                        Description = "Machego Smelly cheese danish fontina.",
+                        ImagePath = "hotdog1",
+                        Available = true,
+                        PrepTime = 10,
+                        Ingredients = new List<string>() {"Regular bun", "Sausage", "Ketchup" },
+                        Price = 8,
+                        IsFavorite = true
+                    },
+                    new HotDog()
+                    {
+                        HotDogId = 2,
+                        Name = "Haute Dog",
+                        ShortDescription = "The classy one",
+                        Description = "Bacon ipsum dolor amet",
+                        ImagePath = "hotdog2",
+                        Available = true,
+                        PrepTime = 15,
+                        Ingredients = new List<string>() {"Baked bun", "Gourmet Sausage" },
+                        Price = 10,
+                        IsFavorite = false
+                    }
+                }
+            },
+            new HotDogGroup()
+            {
+                HotDogGroupID = 2, Title = "Veggie Lovers", ImagePath = "", HotDogs = new List<HotDog>()
+                {
+                    new HotDog()
+                    {
+                        HotDogId = 3, 
+                        Name = "Veggie Hot Dog",
+                        ShortDescription = "American for non meat-lovers",
+                        Description = "Veggies es bonus vobis",
+                        ImagePath = "hotdog3",
+                        Available = true,
+                        PrepTime = 10,
+                        Ingredients = new List<string>() {"Bun", "Vegetarian Sausage" },
+                        Price = 8,
+                        IsFavorite = false,
+                    },
+                    new HotDog()
+                    {
+                        HotDogId = 4,
+                        Name = "Haute Dog Veggie",
+                        ShortDescription = "Classy and veggie",
+                        Description = "Turnip greens yarrow ricebean rutabaga",
+                        ImagePath = "hotdog5",
+                        Available = true,
+                        PrepTime = 15,
+                        Ingredients = new List<string>() {"Baked bun", "Gourmet vegetarian sausage" },
+                        Price = 10,
+                        IsFavorite = true
+                    }
+                }
+            }
+        };
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            IEnumerable<HotDog> hotDogs = from hotDogGroup in hotDogGroups
+                                          from hotDog in hotDogGroup.HotDogs
+                                          select hotDog;
+            return hotDogs.ToList();
+        }
+
+        public HotDog GetHotDogById(int hotDogId)
+        {
+            IEnumerable<HotDog> hotDogs = from hotDogGroup in hotDogGroups
+                                          from hotDog in hotDogGroup.HotDogs.Where(dog => dog.HotDogId == hotDogId)
+                                          select hotDog;
+            return hotDogs.FirstOrDefault();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return hotDogGroups;
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+        {
+            var group = hotDogGroups.Where(d => d.HotDogGroupID == hotDogGroupId).FirstOrDefault();
+
+            if (group != null)
+            {
+                return group.HotDogs;
+            }
+            return null;
+        }
+
+        public List<HotDog> GetFavoriteHotDogs()
+        {
+            IEnumerable<HotDog> hotDogs = from hotDogGroup in hotDogGroups
+                                          from hotDog in hotDogGroup.HotDogs.Where(d => d.IsFavorite == true)
+                                          select hotDog;
+            return hotDogs.ToList();
+        }
+    }
+}
