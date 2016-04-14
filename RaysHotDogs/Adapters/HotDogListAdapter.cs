970c6ed6230b25d6da6716bd58bc66236b9fb8b2
sethdorris <sethc.dorris@gmail.com>
2016-04-14 09:36:21 -0600

    Editing the adapter to display data in the new custom row view

--- RaysHotDogs/Adapters/HotDogListAdapter.cs
+++ RaysHotDogs/Adapters/HotDogListAdapter.cs
@@ -51,9 +51,11 @@ namespace RaysHotDogs.Adapters
 
             if (convertView == null)
             {
-                convertView = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+                convertView = context.LayoutInflater.Inflate(Resource.Layout.HotDogRowView, null);
             }
-            convertView.FindViewById<TextView>(Android.Resource.Id.Text1).Text = item.Name;
+            convertView.FindViewById<TextView>(Resource.Id.hotDogNameTextView).Text = item.Name;
+            convertView.FindViewById<TextView>(Resource.Id.shortDescriptionTextView).Text = item.ShortDescription;
+            convertView.FindViewById<TextView>(Resource.Id.priceTextView).Text = "$ " + item.Price;
             return convertView;
         }
     }
