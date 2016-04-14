78d3eec03db1e398a521fed9eeda19dbe7679d28
sethdorris <sethc.dorris@gmail.com>
2016-04-14 11:46:40 -0600

    Built initial Main Menu View

--- RaysHotDogs/Resources/Resource.Designer.cs
+++ RaysHotDogs/Resources/Resource.Designer.cs
@@ -124,6 +124,9 @@ namespace RaysHotDogs
 			// aapt resource value: 0x7f030003
 			public const int Main = 2130903043;
 			
+			// aapt resource value: 0x7f030004
+			public const int MainMenu = 2130903044;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
